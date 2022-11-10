6bc45d334befeb6f8ec8f795afc85955682815ae
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-09 17:37:22 -0700

    Created Function 2

--- apps.js
+++ apps.js
@@ -1,7 +1,28 @@
+
+//  function 1
 function multby5(num1,num2) {
-    let result = (num1 + num2) *5;
-    return result;
+    let result1 = (num1 + num2) *5;
+    return result1;
 }
 
 console.log(multby5(5,2));
 console.log(multby5(24,85));
+console.log(multby5(31278,77458));
+
+// Function 2
+
+function length(string){
+    let result2 = (string.length) >10;
+    return result2; 
+}
+console.log(length(`Jacksonville`));
+console.log(length(`Edmonton`));
+console.log(length(`Saskatchewan`));
+
+function PHString([text]) {
+        let result3 = ([text.startswith(`ph`)]);
+        return result3;
+}
+
+console.log(PHString([`John`, `Jack`, `Phil`, `Bobby`]));
+
